08c959c42ae39ce11536db0080ee086cfa6ff16f
Abhishek <181220003@nitdelhi.ac.in>
2020-01-23 21:18:16 +0530

    initial commit

--- app/src/main/java/com/example/colossustex/homePage/ItemAdapter.kt
+++ app/src/main/java/com/example/colossustex/homePage/ItemAdapter.kt
@@ -1,5 +1,7 @@
 package com.example.colossustex.homePage
 
+import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,17 +10,14 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.firebase.ui.database.FirebaseRecyclerAdapter
 import com.firebase.ui.database.FirebaseRecyclerOptions
-import com.smarteist.autoimageslider.IndicatorAnimations
-import com.smarteist.autoimageslider.SliderAnimations
-import com.smarteist.autoimageslider.SliderLayout
 import com.squareup.picasso.Picasso
 import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.navigation.findNavController
-import com.example.colossustex.MainActivity
 import com.example.colossustex.R
 import com.smarteist.autoimageslider.DefaultSliderView
-import java.security.AccessController.getContext
+import com.smarteist.autoimageslider.IndicatorAnimations
+import com.smarteist.autoimageslider.SliderAnimations
+import com.smarteist.autoimageslider.SliderLayout
 
 
 class ItemAdapter(options: FirebaseRecyclerOptions<Item>) : FirebaseRecyclerAdapter<Item, ItemAdapter.ItemViewHolder>(options) {
@@ -30,13 +29,27 @@ class ItemAdapter(options: FirebaseRecyclerOptions<Item>) : FirebaseRecyclerAdap
     }
 
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int, model: Item) {
+        var context = holder.constraintLayout.context
         holder.description.text = model.description
         holder.heading.text = model.heading
         Picasso.get().load(model.image).into(holder.image)
         holder.constraintLayout.setOnClickListener{
-            Toast.makeText(it.context,"position:${position+1}",Toast.LENGTH_SHORT).show()
+            Toast.makeText(it.context,model.description,Toast.LENGTH_SHORT).show()
         }
+
+        if(position == 0){
+            holder.sliderLayout.visibility = View.VISIBLE
+        }                //To set the sliderLayout only for the first item
+        else{
+            holder.sliderLayout.visibility = View.GONE
+        }
+
+
+
     }
+
+
+
 
 
     class ItemViewHolder(itemView: View):RecyclerView.ViewHolder(itemView){
@@ -44,6 +57,52 @@ class ItemAdapter(options: FirebaseRecyclerOptions<Item>) : FirebaseRecyclerAdap
         val image: ImageView = itemView.findViewById(R.id.imageView_image)
         val heading: TextView = itemView.findViewById(R.id.textView_heading)
         val description: TextView = itemView.findViewById(R.id.textView_description)
-        val constraintLayout: ConstraintLayout = itemView.findViewById(R.id.constraintLayout)
+        val constraintLayout: ConstraintLayout = itemView.findViewById(R.id.constraintLayout1)
+        val sliderLayout : SliderLayout = itemView.findViewById(R.id.imageSlider)
+
+        init{
+
+
+                Log.i("Adapter","visible")
+                sliderLayout .visibility = View.VISIBLE
+                sliderLayout.setIndicatorAnimation(IndicatorAnimations.SWAP); //set indicator animation by using 	 				SliderLayout.IndicatorAnimations. :WORM or THIN_WORM or COLOR or DROP or FILL or NONE or SCALE or SCALE_DOWN or SLIDE and SWAP!!
+                sliderLayout.setSliderTransformAnimation(SliderAnimations.SIMPLETRANSFORMATION)
+                sliderLayout.scrollTimeInSec = 2; //set scroll delay in seconds :
+
+                for (i in 0..3) {
+
+                    val sliderView = DefaultSliderView(constraintLayout.context)
+
+                    when (i) {
+                        0 -> sliderView.imageUrl =
+                            "https://images.pexels.com/photos/547114/pexels-photo-547114.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+                        1 -> sliderView.imageUrl =
+                            "https://images.pexels.com/photos/218983/pexels-photo-218983.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+                        2 -> sliderView.imageUrl =
+                            "https://images.pexels.com/photos/747964/pexels-photo-747964.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"
+                        3 -> sliderView.imageUrl =
+                            "https://images.pexels.com/photos/929778/pexels-photo-929778.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+                    }
+
+                    sliderView.setImageScaleType(ImageView.ScaleType.CENTER_CROP)
+                    sliderView.setOnSliderClickListener {
+                        Toast.makeText(
+                            constraintLayout.context,
+                            "This is slider " + (i + 1),
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+
+                    //at last add this view in your layout :
+                    sliderLayout.addSliderView(sliderView)
+                }
+
+            }
+
     }
+
+
+
 }
+
+
--- app/src/main/java/com/example/colossustex/homePage/homePage.kt
+++ app/src/main/java/com/example/colossustex/homePage/homePage.kt
@@ -1,33 +1,28 @@
 package com.example.colossustex.homePage
 
+
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.ImageView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-
 import com.firebase.ui.database.FirebaseRecyclerOptions
 import com.google.firebase.database.FirebaseDatabase
-import com.smarteist.autoimageslider.IndicatorAnimations
-import com.smarteist.autoimageslider.SliderAnimations
-import com.smarteist.autoimageslider.SliderLayout
-import android.widget.Toast
 import com.example.colossustex.R
-import com.smarteist.autoimageslider.SliderView
-import com.smarteist.autoimageslider.DefaultSliderView
+
 
 
 class homePage : Fragment() {
 
     lateinit var recyclerView: RecyclerView
     lateinit var adapter: ItemAdapter
-    lateinit var sliderLayout: SliderLayout
 
     private lateinit var viewModel: HomePageViewModel
+
+
+
+
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
@@ -42,12 +37,6 @@ class homePage : Fragment() {
         adapter = ItemAdapter(options)
         recyclerView.adapter = adapter
 
-        sliderLayout = lay.findViewById(R.id.imageSlider);
-        sliderLayout.setIndicatorAnimation(IndicatorAnimations.SWAP); //set indicator animation by using 	 				SliderLayout.IndicatorAnimations. :WORM or THIN_WORM or COLOR or DROP or FILL or NONE or SCALE or SCALE_DOWN or SLIDE and SWAP!!
-        sliderLayout.setSliderTransformAnimation(SliderAnimations.SIMPLETRANSFORMATION)
-        sliderLayout.scrollTimeInSec = 2; //set scroll delay in seconds :
-
-        setSliderViews();
 
 
 
@@ -57,35 +46,7 @@ class homePage : Fragment() {
         return lay
     }
 
-    private fun setSliderViews() {
-        for (i in 0..3) {
 
-            val sliderView = DefaultSliderView(context)
-
-            when (i) {
-                0 -> sliderView.imageUrl =
-                    "https://images.pexels.com/photos/547114/pexels-photo-547114.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
-                1 -> sliderView.imageUrl =
-                    "https://images.pexels.com/photos/218983/pexels-photo-218983.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
-                2 -> sliderView.imageUrl =
-                    "https://images.pexels.com/photos/747964/pexels-photo-747964.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"
-                3 -> sliderView.imageUrl =
-                    "https://images.pexels.com/photos/929778/pexels-photo-929778.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
-            }
-
-            sliderView.setImageScaleType(ImageView.ScaleType.CENTER_CROP)
-            sliderView.setOnSliderClickListener {
-                Toast.makeText(
-                    context,
-                    "This is slider " + (i + 1),
-                    Toast.LENGTH_SHORT
-                ).show()
-            }
-
-            //at last add this view in your layout :
-            sliderLayout.addSliderView(sliderView)
-        }
-    }
 
     override fun onStart() {
         super.onStart()
