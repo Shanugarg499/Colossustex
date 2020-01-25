f01b947e45873ee6b7c8977f55c329397f92f795
Abhishek <181220003@nitdelhi.ac.in>
2020-01-25 15:33:11 +0530

    initial commit

--- app/src/main/java/com/example/colossustex/homePage/HomePage.kt
+++ app/src/main/java/com/example/colossustex/homePage/HomePage.kt
@@ -4,7 +4,9 @@ package com.example.colossustex.homePage
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.view.*
+import android.widget.Toast
 import android.widget.Toolbar
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -13,8 +15,7 @@ import com.google.firebase.database.FirebaseDatabase
 import com.example.colossustex.R
 
 
-
-class homePage : Fragment() {
+class HomePage : Fragment() {
 
     lateinit var recyclerView: RecyclerView
     lateinit var adapter: ItemAdapter
@@ -22,8 +23,11 @@ class homePage : Fragment() {
     private lateinit var viewModel: HomePageViewModel
 
 
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
 
         val lay = inflater.inflate(R.layout.home_page_fragment, container, false)
         viewModel = ViewModelProviders.of(this).get(HomePageViewModel::class.java)
@@ -38,13 +42,26 @@ class homePage : Fragment() {
 
         val toolbar = lay.findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbar)
         toolbar.inflateMenu(R.menu.main_menu)
+        toolbar.setOnMenuItemClickListener {
+            var toastMessage = ""
+            when (it.itemId) {
+                R.id.notification_Settings -> toastMessage = "Notification Settings"
+                R.id.edit_profile -> toastMessage = "Edit Profile"
+                R.id.change_password -> toastMessage = "Change Password"
+                R.id.spinning_mill -> toastMessage = "Spinning mill?"
+                R.id.support -> toastMessage = "Support"
+                R.id.advertise_with_us -> toastMessage = "Advertise With Us"
+                R.id.rate_this_app -> toastMessage = "Rate This App"
+                R.id.share_app -> toastMessage = "Share App"
+            }
+            Toast.makeText( context , toastMessage , Toast.LENGTH_SHORT).show()
+            true
+        }
 
 
 
         return lay
     }
-
-
 
 
     override fun onStart() {
@@ -56,7 +73,6 @@ class homePage : Fragment() {
         super.onStop()
         adapter.stopListening()
     }
-
 
 
 }
