6d02064066f46ba2bbedc0c359e177936cb1bcfa
Abhishek <181220003@nitdelhi.ac.in>
2020-02-03 21:09:56 +0530

    added navigation from home page to spinning mill of india section

--- app/src/main/java/com/example/colossustex/homePage/ItemAdapter.kt
+++ app/src/main/java/com/example/colossustex/homePage/ItemAdapter.kt
@@ -13,14 +13,17 @@ import com.firebase.ui.database.FirebaseRecyclerOptions
 import com.squareup.picasso.Picasso
 import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.navigation.findNavController
 import com.example.colossustex.R
 import com.smarteist.autoimageslider.DefaultSliderView
 import com.smarteist.autoimageslider.IndicatorAnimations
 import com.smarteist.autoimageslider.SliderAnimations
 import com.smarteist.autoimageslider.SliderLayout
+import kotlinx.coroutines.internal.artificialFrame
 
 
-class ItemAdapter(options: FirebaseRecyclerOptions<Item>) : FirebaseRecyclerAdapter<Item, ItemAdapter.ItemViewHolder>(options) {
+class ItemAdapter(options: FirebaseRecyclerOptions<Item>) :
+    FirebaseRecyclerAdapter<Item, ItemAdapter.ItemViewHolder>(options) {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
         return ItemViewHolder(
             LayoutInflater.from(parent.context)
@@ -32,74 +35,72 @@ class ItemAdapter(options: FirebaseRecyclerOptions<Item>) : FirebaseRecyclerAdap
         holder.description.text = model.description
         holder.heading.text = model.heading
         Picasso.get().load(model.image).into(holder.image)
-        holder.constraintLayout.setOnClickListener{
-            Toast.makeText(it.context,model.description,Toast.LENGTH_SHORT).show()
+        holder.constraintLayout.setOnClickListener {
+            when (position) {
+                0 -> holder.constraintLayout.findNavController().navigate(HomePageDirections.actionHomePageToSpinningMillOfIndia())
+                else -> Toast.makeText(it.context, model.description, Toast.LENGTH_SHORT).show()
+            }
         }
 
-        if(position == 0){
+        if (position == 0) {
             holder.sliderLayout.visibility = View.VISIBLE
         }                //To set the sliderLayout only for the first item
-        else{
+        else {
             holder.sliderLayout.visibility = View.GONE
         }
-
 
 
     }
 
 
-
-
-
-    class ItemViewHolder(itemView: View):RecyclerView.ViewHolder(itemView) {
+    class ItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
         val image: ImageView = itemView.findViewById(R.id.imageView_image)
         val heading: TextView = itemView.findViewById(R.id.textView_heading)
         val description: TextView = itemView.findViewById(R.id.textView_description)
         val constraintLayout: ConstraintLayout = itemView.findViewById(R.id.constraintLayout1)
-        val sliderLayout : SliderLayout = itemView.findViewById(R.id.imageSlider)
+        val sliderLayout: SliderLayout = itemView.findViewById(R.id.imageSlider)
 
-        init{
+        init {
 
 
-                Log.i("Adapter","visible")
-                sliderLayout .visibility = View.VISIBLE
-                sliderLayout.setIndicatorAnimation(IndicatorAnimations.SWAP) //set indicator animation by using 	 				SliderLayout.IndicatorAnimations. :WORM or THIN_WORM or COLOR or DROP or FILL or NONE or SCALE or SCALE_DOWN or SLIDE and SWAP!!
-                sliderLayout.setSliderTransformAnimation(SliderAnimations.SIMPLETRANSFORMATION)
-                sliderLayout.scrollTimeInSec = 2; //set scroll delay in seconds :
+            Log.i("Adapter", "visible")
+            sliderLayout.visibility = View.VISIBLE
+            sliderLayout.setIndicatorAnimation(IndicatorAnimations.SWAP) //set indicator animation by using 	 				SliderLayout.IndicatorAnimations. :WORM or THIN_WORM or COLOR or DROP or FILL or NONE or SCALE or SCALE_DOWN or SLIDE and SWAP!!
+            sliderLayout.setSliderTransformAnimation(SliderAnimations.SIMPLETRANSFORMATION)
+            sliderLayout.scrollTimeInSec = 2; //set scroll delay in seconds :
 
-                for (i in 0..3) {
+            for (i in 0..3) {
 
-                    val sliderView = DefaultSliderView(constraintLayout.context)
+                val sliderView = DefaultSliderView(constraintLayout.context)
 
-                    when (i) {
-                        0 -> sliderView.imageUrl =
-                            "https://images.pexels.com/photos/547114/pexels-photo-547114.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
-                        1 -> sliderView.imageUrl =
-                            "https://images.pexels.com/photos/218983/pexels-photo-218983.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
-                        2 -> sliderView.imageUrl =
-                            "https://images.pexels.com/photos/747964/pexels-photo-747964.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"
-                        3 -> sliderView.imageUrl =
-                            "https://images.pexels.com/photos/929778/pexels-photo-929778.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
-                    }
-
-                    sliderView.setImageScaleType(ImageView.ScaleType.CENTER_CROP)
-                    sliderView.setOnSliderClickListener {
-                        Toast.makeText(
-                            constraintLayout.context,
-                            "This is slider " + (i + 1),
-                            Toast.LENGTH_SHORT
-                        ).show()
-                    }
-
-                    //at last add this view in your layout :
-                    sliderLayout.addSliderView(sliderView)
+                when (i) {
+                    0 -> sliderView.imageUrl =
+                        "https://images.pexels.com/photos/547114/pexels-photo-547114.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+                    1 -> sliderView.imageUrl =
+                        "https://images.pexels.com/photos/218983/pexels-photo-218983.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+                    2 -> sliderView.imageUrl =
+                        "https://images.pexels.com/photos/747964/pexels-photo-747964.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"
+                    3 -> sliderView.imageUrl =
+                        "https://images.pexels.com/photos/929778/pexels-photo-929778.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
                 }
 
+                sliderView.setImageScaleType(ImageView.ScaleType.CENTER_CROP)
+                sliderView.setOnSliderClickListener {
+                    Toast.makeText(
+                        constraintLayout.context,
+                        "This is slider " + (i + 1),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+
+                //at last add this view in your layout :
+                sliderLayout.addSliderView(sliderView)
             }
 
-    }
+        }
 
+    }
 
 
 }
