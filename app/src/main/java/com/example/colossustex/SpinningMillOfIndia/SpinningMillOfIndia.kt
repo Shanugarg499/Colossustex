a2a7584693ac8732e9dc846cec5a9a14a64ff033
Abhishek <181220003@nitdelhi.ac.in>
2020-02-04 22:54:02 +0530

    added shadow effect and some headings with references

--- app/src/main/java/com/example/colossustex/SpinningMillOfIndia/SpinningMillOfIndia.kt
+++ app/src/main/java/com/example/colossustex/SpinningMillOfIndia/SpinningMillOfIndia.kt
@@ -7,20 +7,37 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.RecyclerView
 
 import com.example.colossustex.R
+import org.w3c.dom.Text
 
 class SpinningMillOfIndia : Fragment() {
 
+    private lateinit var recyclerView: RecyclerView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        var lay =  inflater.inflate(R.layout.fragment_spinning_mill_of_india, container, false)
-        val toolbar = lay.findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbar_spinning_mills_in_india)
+        var lay = inflater.inflate(R.layout.fragment_spinning_mill_of_india, container, false)
+        val toolbar =
+            lay.findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbar_spinning_mills_in_india)
+        val cotton = lay.findViewById<TextView>(R.id.textView_cotton)
+        val synthetic = lay.findViewById<TextView>(R.id.textView_Synthetic)
+        val viscose = lay.findViewById<TextView>(R.id.textView_viscose)
+        val texturised = lay.findViewById<TextView>(R.id.textView_texturised)
+        val fancy = lay.findViewById<TextView>(R.id.textView_fancy)
+        val postYarnRequirement = lay.findViewById<CardView>(R.id.cardView_post_yarn_requirement)
+        val directMillAgentsandTraders =
+            lay.findViewById<CardView>(R.id.cardView_direct_mill_agent_and_traders)
+
+
+
         return lay
     }
 
