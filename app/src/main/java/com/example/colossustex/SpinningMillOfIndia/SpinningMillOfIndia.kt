c91fcad49f8a6b5029f78888ddc452a2fffd808b
Abhishek <181220003@nitdelhi.ac.in>
2020-02-11 23:03:05 +0530

    added on scroll listener for recycler in maills in india fragment to show a toast when we scroll through 1 item

--- app/src/main/java/com/example/colossustex/SpinningMillOfIndia/SpinningMillOfIndia.kt
+++ app/src/main/java/com/example/colossustex/SpinningMillOfIndia/SpinningMillOfIndia.kt
@@ -6,13 +6,14 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.SCROLL_STATE_IDLE
 import com.example.colossustex.R
-import com.google.android.material.appbar.AppBarLayout
 import com.google.firebase.database.*
 
 class SpinningMillOfIndia : Fragment() {
@@ -21,6 +22,8 @@ class SpinningMillOfIndia : Fragment() {
     private lateinit var mDb: DatabaseReference
     private lateinit var posts: ArrayList<post>
     private lateinit var adapter: PostAdapter
+    private lateinit var manager: LinearLayoutManager
+    private var done = false
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -41,6 +44,7 @@ class SpinningMillOfIndia : Fragment() {
         val upButton = lay.findViewById<ImageView>(R.id.upButtonSpinningMillsOfIndia)
         val homeButton = lay.findViewById<ImageView>(R.id.imageView_home_page)
 
+        manager = LinearLayoutManager(context)
 
 
 
@@ -56,7 +60,29 @@ class SpinningMillOfIndia : Fragment() {
 
 
         recyclerView = lay.findViewById(R.id.recylerView_spinning_mills_of_india)
-        recyclerView.layoutManager = LinearLayoutManager(context)
+        recyclerView.layoutManager = manager
+        recyclerView.addOnScrollListener(
+            object : RecyclerView.OnScrollListener() {
+                override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                    super.onScrollStateChanged(recyclerView, newState)
+
+                    if (!done && manager.findFirstVisibleItemPosition() >= 1 && newState == SCROLL_STATE_IDLE) {
+                        Toast.makeText(context, "dommb", Toast.LENGTH_SHORT).show()
+                        done = true
+                    }
+                    if(done &&  manager.findFirstVisibleItemPosition() == 0 && newState == SCROLL_STATE_IDLE){
+                        done = false
+                    }
+
+                }
+            }
+        )
+
+
+
+
+
+
 
         posts = ArrayList()
 
